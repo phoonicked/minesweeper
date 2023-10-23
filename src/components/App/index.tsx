fc71f56d2c6c8826a1334ce5a88812d82c5582cf
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2023-10-23 16:11:40 +0100

    Fixed syntax due to compilation error caused by prettier

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import "./App.scss";
 
 const App: React.FC = () => {
-    return(
-        <div className="App">
-            Hello World!
-        </div>
-    )
+    return <div className="App">Hello World!</div>;
 };
 
 export default App;
