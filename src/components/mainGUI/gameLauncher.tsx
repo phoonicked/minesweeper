2562b345526e6f95ba22511019e0ebad90de5f94
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2023-10-26 13:41:38 +0100

    Renamed App.scss to index.scss and index.tsx to gameLauncher.tsx to properly reflect entry point of the program

--- src/components/mainGUI/gameLauncher.tsx
+++ src/components/mainGUI/gameLauncher.tsx
@@ -6,7 +6,11 @@ const App: React.FC = () => {
     return (
         <div className="App">
             <div className="Header">
-                <NumberDisplay value={100}/>
+                <NumberDisplay value={0}/>
+                <div className="Buttons">
+                    Reset
+                </div>
+                <NumberDisplay value={0}/>
             </div>
             <div className="Body">Body</div>
         </div>
